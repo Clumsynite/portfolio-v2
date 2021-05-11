c57244c1f07a2172dcb0357b7b70b14e4e86d322
Rishabh Pathak <clusmsyknight@gmail.com>
2021-05-11 20:09:39 +0530

    added footer

---
+++ components/Footer.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { Icon } from "semantic-ui-react";
+import styles from "../styles/Footer.module.css";
+
+export default function Footer({ dark, mobile }) {
+  return (
+    <footer
+      className={styles.footer}
+      style={{ marginTop: mobile ? 100 : null }}
+    >
+      Made with
+      <span style={{ padding: "0px 4px" }}>
+        <Icon name={"heart"} color="red" fitted inverted={dark} />
+      </span>
+      by
+      <a
+        aria-label="Link to Github Profile"
+        href="https://github.com/Clumsynite"
+        title="Github Profile"
+        className="link"
+        target="_blank"
+        rel="noreferrer"
+        style={{ paddingLeft: 4 }}
+      >
+        Rishabh Pathak
+      </a>
+    </footer>
+  );
+}
