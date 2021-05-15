27015828f4c1f0b448bae00a78f22da1a3b42547
Rishabh Pathak <clusmsyknight@gmail.com>
2021-05-15 18:00:32 +0530

    used custom document to load cdn at the start

---
+++ pages/_document.js
@@ -0,0 +1,25 @@
+import Document, { Html, Head, Main, NextScript } from "next/document";
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
+
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          <title>Rishabh's Portflio </title>
+          <script src="https://code.iconify.design/1/1.0.7/iconify.min.js"></script>
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
