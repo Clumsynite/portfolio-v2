9358c9e7a5db9c8b8bff5e0b0073ff7fb9dea1fc
Rishabh Pathak <clusmsyknight@gmail.com>
2021-05-16 16:25:22 +0530

    added title for popup

--- components/ThemeSwitcher.js
+++ components/ThemeSwitcher.js
@@ -1,26 +1,29 @@
 import React, { useContext } from "react";
 import { ThemeContext } from "../context/Theme";
 import { Icon } from "semantic-ui-react";
+import { Popup } from "./CommonComponents";
 
 export default function ThemeSwitcher() {
   const { theme, setTheme } = useContext(ThemeContext);
 
   const icon = {
-    dark: { name: "moon", color: "black" },
-    light: { name: "sun", color: "yellow" },
+    dark: { name: "moon", color: "black", title: "Dark Mode" },
+    light: { name: "sun", color: "yellow", title: "Light Mode" },
   };
 
   const nextTheme = () => (theme === "light" ? "dark" : "light");
 
   return (
-    <Icon
-      onClick={() => setTheme(nextTheme())}
-      name={icon[nextTheme()].name}
-      circular
-      flipped={"horizontally"}
-      link
-      color={icon[nextTheme()].color}
-      inverted
-    />
+    <Popup dark={theme === "dark"} content={icon[nextTheme()].title}>
+      <Icon
+        onClick={() => setTheme(nextTheme())}
+        name={icon[nextTheme()].name}
+        circular
+        flipped={"horizontally"}
+        link
+        color={icon[nextTheme()].color}
+        inverted
+      />
+    </Popup>
   );
 }
