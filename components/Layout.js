2076040c088b67cb798bd62a7d8d60114b6d9fc6
Rishabh Pathak <clusmsyknight@gmail.com>
2021-05-16 16:26:48 +0530

    return only on mount
    causes blink on navigation

--- components/Layout.js
+++ components/Layout.js
@@ -1,33 +1,40 @@
 import Head from "next/head";
-import React from "react";
+import React, { useState, useEffect, useContext } from "react";
 import { Container } from "semantic-ui-react";
 
 import Navbar from "./Navbar";
+import { ThemeContext } from "../context/Theme";
 
 const name = "Rishabh Pathak";
 export const siteTitle = `${name} Portfolio`;
 
 export default function Layout({ children, page }) {
+  const { theme } = useContext(ThemeContext);
+  const [mounted, setMounted] = useState(false);
+  useEffect(() => setMounted(true), []);
+
   return (
-    <div>
-      <Head>
-        <link rel="icon" href="/favicon.ico" />
-        <meta
-          name="description"
-          content={`${name}'s Web development Portfolio. Built with Next.js`}
-        />
-        {/* <meta
+    mounted && (
+      <div style={{ transition: "0.3s linear" }}>
+        <Head>
+          <link rel="icon" href="/favicon.ico" />
+          <meta
+            name="description"
+            content={`${name}'s Web development Portfolio. Built with Next.js`}
+          />
+          {/* <meta
           property="og:image"
           content={`link to the image here`}
         /> */}
-        <meta name="og:title" content={siteTitle} />
-        <meta name="twitter:card" content="summary_large_image" />
-      </Head>
-      <Navbar page={page}>
-        <Container fluid>
-          <main>{children}</main>
-        </Container>
-      </Navbar>
-    </div>
+          <meta name="og:title" content={siteTitle} />
+          <meta name="twitter:card" content="summary_large_image" />
+        </Head>
+        <Navbar page={page} dark={theme === "dark"}>
+          <Container fluid>
+            <main>{children}</main>
+          </Container>
+        </Navbar>
+      </div>
+    )
   );
 }
