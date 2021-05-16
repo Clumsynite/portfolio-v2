7058494a740219d392985ef5641fd101253a80e9
Rishabh Pathak <clusmsyknight@gmail.com>
2021-05-16 16:26:16 +0530

    imported semantic ui css before custom global
    might work, not sure

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,14 @@
 import Head from "next/head";
+import "semantic-ui-css/semantic.min.css";
 
 import "../styles/globals.css";
-import "semantic-ui-css/semantic.min.css";
 import { ThemeProvider } from "../context/Theme";
 
 function MyApp({ Component, pageProps }) {
   return (
     <ThemeProvider>
       <Head>
-        <script src="https://code.iconify.design/1/1.0.7/iconify.min.js"></script>
+        <script defer src="https://code.iconify.design/1/1.0.7/iconify.min.js"></script>
       </Head>
       <Component {...pageProps} />
     </ThemeProvider>
