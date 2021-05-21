6bb4485a5a41ad827a440f7d49cdff855088d4c2
Rishabh Pathak <clusmsyknight@gmail.com>
2021-05-21 21:18:58 +0530

    render every project

--- components/ProjectGrid.js
+++ components/ProjectGrid.js
@@ -27,17 +27,19 @@ export const ProjectCard = ({ project, dark }) => {
   return (
     <div className={styles.container}>
       <div className={styles.box}>
-        <div className={styles.thumbnail}>
-          <Image
-            src={png}
-            className={styles.image}
-            height={imageSize.height}
-            width={imageSize.width}
-            objectFit="cover"
-            alt={title}
-            title={`${title} - Screenshot`}
-          />
-        </div>
+        {png && (
+          <div className={styles.thumbnail}>
+            <Image
+              src={png}
+              className={styles.image}
+              height={imageSize.height}
+              width={imageSize.width}
+              objectFit="cover"
+              alt={title}
+              title={`${title} - Screenshot`}
+            />
+          </div>
+        )}
         <div className={styles.content}>
           <h2 className={styles.name}>{title}</h2>
           <div className={styles.desc}>
@@ -47,38 +49,42 @@ export const ProjectCard = ({ project, dark }) => {
             <Tags languages={languages} title={title} size={28} />
           </div>
           <div className={styles.links}>
-            <ExtLink
-              dark={dark}
-              name={
-                <Button
-                  inverted
-                  color="teal"
-                  content="Repo"
-                  icon="github"
-                  labelPosition="left"
-                  inverted={dark}
-                  basic={!dark}
-                />
-              }
-              title={`Link to ${title}'s Github Repo`}
-              to={repo}
-            />
-            <ExtLink
-              dark={dark}
-              name={
-                <Button
-                  inverted
-                  color="blue"
-                  content="Live"
-                  labelPosition="right"
-                  icon="eye"
-                  inverted={dark}
-                  basic={!dark}
-                />
-              }
-              title={`Link to ${title}'s Homepage`}
-              to={live}
-            />
+            {repo && (
+              <ExtLink
+                dark={dark}
+                name={
+                  <Button
+                    inverted
+                    color="teal"
+                    content="Repo"
+                    icon="github"
+                    labelPosition="left"
+                    inverted={dark}
+                    basic={!dark}
+                  />
+                }
+                title={`Link to ${title}'s Github Repo`}
+                to={repo}
+              />
+            )}
+            {live && (
+              <ExtLink
+                dark={dark}
+                name={
+                  <Button
+                    inverted
+                    color="blue"
+                    content="Live"
+                    labelPosition="right"
+                    icon="eye"
+                    inverted={dark}
+                    basic={!dark}
+                  />
+                }
+                title={`Link to ${title}'s Homepage`}
+                to={live}
+              />
+            )}
           </div>
         </div>
       </div>
@@ -90,12 +96,9 @@ const ProjectGrid = ({ projects, dark }) => {
   return (
     <ResponsiveMasonry columnsCountBreakPoints={{ 350: 1, 750: 2, 900: 3 }}>
       <Masonry gutter={"10px"}>
-        {projects.map(
-          (project, index) =>
-            project?.png && (
-              <ProjectCard key={index} project={project} dark={dark} />
-            )
-        )}
+        {projects.map((project, index) => (
+          <ProjectCard key={index} project={project} dark={dark} />
+        ))}
       </Masonry>
     </ResponsiveMasonry>
   );
