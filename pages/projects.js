da401c83c8c3133d6d3cb2f36ee47435f4105cc7
Rishabh Pathak <clusmsyknight@gmail.com>
2021-05-22 13:48:56 +0530

    added filter for projects

--- pages/projects.js
+++ pages/projects.js
@@ -1,22 +1,73 @@
-import React, { useContext } from "react";
+import React, { useState, useEffect, useContext } from "react";
 import Head from "next/head";
-
+import { Dropdown } from "semantic-ui-react";
+import { intersection } from "lodash";
 import styles from "../styles/Projects.module.css";
 import Layout from "../components/Layout";
 import { ThemeContext } from "../context/Theme";
-import projects from "../config/projects";
 import ProjectGrid from "../components/ProjectGrid";
+import projectList from "../config/projects";
+import languageList from "../config/languages";
+import LanguageIcon from "../components/LanguageIcons";
 
 export async function getStaticProps() {
   return {
     props: {
-      projects,
+      projectList,
+      languageList,
     },
   };
 }
 
-const Projects = ({ projects }) => {
+const Projects = ({ projectList, languageList }) => {
   const { theme } = useContext(ThemeContext);
+
+  const [projects, setProjects] = useState(projectList);
+  const [languages, setLanguages] = useState([]);
+
+  useEffect(() => {
+    let languages = [];
+    _.forIn(projectList, (value) => {
+      languages = [...languages, ..._.get(value, "languages", [])];
+    });
+    languages = _.uniq(languages);
+    for (let i = 0; i < languages.length; i++) {
+      const language = languages[i];
+      const object = {
+        text: (
+          <span>
+            <LanguageIcon
+              language={language}
+              size={20}
+              dark={theme === "dark"}
+            />
+            <span style={{ paddingLeft: 10 }}>
+              {_.get(languageList, `${language}.name`, " ")}
+            </span>
+          </span>
+        ),
+        key: language,
+        value: language,
+      };
+
+      languages[i] = object;
+    }
+    setLanguages([...languages]);
+  }, []);
+
+  const filterProjects = (_, data) => {
+    const { value } = data;
+    if (value.length < 1) {
+      setProjects(projectList);
+    } else {
+      const projectsArray = [];
+      for (let project of projectList) {
+        if (intersection(project.languages, value).length > 0)
+          projectsArray.push(project);
+      }
+      setProjects([...projectsArray]);
+    }
+  };
 
   return (
     <Layout page={"Projects"}>
@@ -30,8 +81,19 @@ const Projects = ({ projects }) => {
           <link rel="icon" href="/favicon.ico" />
         </Head>
         <main>
-          <h1 className={styles.heading}>My Projects</h1>
-          <ProjectGrid projects={projects} dark={theme === "dark"} />
+          <div className={styles.filter}>
+            <Dropdown
+              placeholder="Project built using ..."
+              fluid
+              multiple
+              selection
+              options={languages}
+              onChange={filterProjects}
+            />
+          </div>
+          <div className={styles.projectsGrid}>
+            <ProjectGrid projects={projects} dark={theme === "dark"} />
+          </div>
         </main>
       </div>
     </Layout>
