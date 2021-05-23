2d844eb3404888d8da53fbba4619fbda44804fad
Rishabh Pathak <clusmsyknight@gmail.com>
2021-05-23 21:14:04 +0530

    added contact form
    mail works successfully

---
+++ components/ContactForm.jsx
@@ -0,0 +1,143 @@
+/* eslint-disable no-shadow */
+import { bool } from "prop-types";
+import React, { useState } from "react";
+import { Button, Form, Message } from "semantic-ui-react";
+
+import styles from "../styles/Contact.module.css";
+
+const ContactForm = ({ dark }) => {
+  const [loading, setLoading] = useState(false);
+  const initialValues = { name: "", email: "", message: "" };
+  const initialError = { error: false, title: "", content: "" };
+  const initialSuccess = { success: false, title: "", content: "" };
+  const [values, setValues] = useState(initialValues);
+  const [error, setError] = useState(initialError);
+  const [success, setSuccess] = useState(initialSuccess);
+
+  const handleChange = (_, { name, value }) => {
+    if (success.success) setSuccess(initialSuccess);
+    if (error.error) setError(initialError);
+    setValues({ ...values, [name]: value });
+  };
+
+  const sendMail = async (object) => {
+    try {
+      const response = await fetch(
+        "https://clumsy-gmail.herokuapp.com/send/v2",
+        {
+          method: "POST",
+          body: JSON.stringify(object),
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      return error;
+    }
+  };
+
+  const showSuccess = () =>
+    setSuccess({
+      success: true,
+      title: "Message sent successfully",
+      content: "Hey there, I have received your message",
+    });
+
+  const showError = () =>
+    setError({
+      error: true,
+      title: "Message Failed to send",
+      content:
+        "Oops! Sorry for the error. Till then, you can contact me on any of the links below.",
+    });
+
+  const handleSubmit = async (e) => {
+    try {
+      e.preventDefault();
+      setLoading(true);
+      const data = await sendMail({
+        ...values,
+        website: "https://rishabhpathak.vercel.app/contact",
+        dark,
+      });
+      setLoading(false);
+      if (data.err) {
+        showError();
+      } else {
+        showSuccess();
+        setValues(initialValues);
+      }
+    } catch (error) {
+      setLoading(false);
+      showError();
+      // console.error("Error sending mail", error)
+    }
+  };
+
+  return (
+    <div className={styles.form}>
+      <Form
+        loading={loading}
+        inverted={dark}
+        onSubmit={handleSubmit}
+        size="large"
+        success={success.success}
+        error={error.error}
+      >
+        <Form.Group widths="equal">
+          <Form.Input
+            id="name"
+            name="name"
+            label="Enter your name:"
+            placeholder="Your name?"
+            value={values.name || ""}
+            onChange={handleChange}
+            // required
+          />
+          <Form.Input
+            id="email"
+            name="email"
+            label="Enter your email address:"
+            placeholder="Your email?"
+            value={values.email || ""}
+            onChange={handleChange}
+            // required
+            type="email"
+          />
+        </Form.Group>
+        <Form.TextArea
+          id="message"
+          name="message"
+          label="Message"
+          placeholder="Wanna message me something...?"
+          value={values.message || ""}
+          onChange={handleChange}
+          rows={5}
+          // required
+        />
+        <Button
+          type="submit"
+          content="Message Rishabh"
+          inverted={dark}
+          // secondary
+          size="large"
+          fluid
+          loading={loading}
+        />
+        {success.success && (
+          <Message success header={success.title} content={success.content} />
+        )}
+        {error.error && (
+          <Message error header={error.title} content={error.content} />
+        )}
+      </Form>
+    </div>
+  );
+};
+ContactForm.propTypes = {
+  dark: bool.isRequired,
+};
+export default ContactForm;
--- pages/contact.jsx
+++ pages/contact.jsx
@@ -1,21 +1,31 @@
-import React from 'react'
+import React, { useContext } from "react";
 import Head from "next/head";
-import styles from "../styles/Common.module.css";
-import Layout from "../components/Layout";
 
-const Contact = () => (
+import styles from "../styles/Contact.module.css";
+import Layout from "../components/Layout";
+import ContactForm from "../components/ContactForm";
+import { ThemeContext } from "../context/Theme";
+
+const Contact = () => {
+  const { theme } = useContext(ThemeContext);
+  return (
     <Layout page="Contact">
       <div className={styles.container}>
         <Head>
-          <title>Rishabh Pathak&apos;s Portfolio - Contact</title>
-          <meta name="description" content="Ways to contact Rishabh Pathak" />
+          <title>Rishabh Pathak&apos;s Portfolio - Contact Page</title>
+          <meta
+            name="description"
+            content="Ways to contact Rishabh Pathak. Contact form on this page and other links."
+          />
           <link rel="icon" href="/favicon.ico" />
         </Head>
-        <main className={styles.main}>
-          <h1 className={styles.title}>Contact Page</h1>
+        <main>
+          <h1 className={styles.heading}>Contact me</h1>
+          <ContactForm dark={theme === "dark"} />
         </main>
       </div>
     </Layout>
   );
+};
 
 export default Contact;
