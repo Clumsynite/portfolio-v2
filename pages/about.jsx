3a2c567bb11b95665ebb7ed94aa0aca0417c82c3
Rishabh Pathak <clusmsyknight@gmail.com>
2021-05-30 18:08:39 +0530

    updated about page to render resume

--- pages/about.jsx
+++ pages/about.jsx
@@ -1,7 +1,7 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import Head from "next/head";
 import Link from "next/link";
-import { Icon } from "semantic-ui-react";
+import { Button, Dimmer, Icon, Loader, Segment } from "semantic-ui-react";
 import { arrayOf } from "prop-types";
 
 import styles from "../styles/About.module.css";
@@ -21,6 +21,7 @@ export async function getStaticProps() {
 
 const About = ({ jobs }) => {
   const { theme } = useContext(ThemeContext);
+  const [loading, setLoading] = useState(true);
   return (
     <Layout page="About">
       <div className={styles.container}>
@@ -34,7 +35,7 @@ const About = ({ jobs }) => {
         </Head>
         <main>
           <div className="heading">About ME</div>
-          <section>
+          <section id="about-me">
             <div className={styles["about-me"]} id="me">
               I am <span className={styles.name}>Rishabh Jitendra Pathak</span>,
               a{" "}
@@ -70,7 +71,47 @@ const About = ({ jobs }) => {
               from scratch.
             </div>
           </section>
-          <section>{jobs.length > 0 && <WorkExperience jobs={jobs} />}</section>
+          <section className={styles["work-resume"]}>
+            {jobs.length > 0 && <WorkExperience jobs={jobs} />}
+            <div className={styles.resume}>
+              <Dimmer.Dimmable
+                as={Segment}
+                blurring
+                dimmed={loading}
+                inverted={theme === "dark"}
+              >
+                <Dimmer active={loading} inverted={theme !== "dark"}>
+                  <Loader>Loading Resume...</Loader>
+                </Dimmer>
+
+                <div className={styles.image}>
+                  <ExtLink
+                    to="/resume.jpg"
+                    name={
+                      <img
+                        src="/resume.jpg"
+                        alt="Rishabh Pathak's Resume"
+                        style={{ height: "100%", width: "100%" }}
+                        onLoad={() => setLoading(false)}
+                      />
+                    }
+                    dark={theme === "dark"}
+                    title="Open as Image in new tab"
+                  />
+                </div>
+                <ExtLink
+                  to="/resume.pdf"
+                  name={
+                    <Button loading={loading} primary fluid>
+                      View as PDF
+                    </Button>
+                  }
+                  dark={theme === "dark"}
+                  title="Open as PDF in new tab"
+                />
+              </Dimmer.Dimmable>
+            </div>
+          </section>
         </main>
       </div>
     </Layout>
