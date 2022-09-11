313dc42b148e84845dc5cf8e53e68f476bf89737
Rishabh Pathak <clusmsyknight@gmail.com>
2022-09-11 20:59:33 +0530

    updated year

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -27,7 +27,7 @@ export default function Footer({ dark }) {
         style={{ paddingLeft: 4 }}
         className="link"
       />
-      <span style={{ paddingLeft: 4 }}>&copy; 2021</span>
+      <span style={{ paddingLeft: 4 }}>&copy; 2022</span>
     </footer>
   );
 }
