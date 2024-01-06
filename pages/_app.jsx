764960ad5f8d1d39c54718de19f0b4c3a32fb4eb
Rishabh Pathak <clusmsyknight@gmail.com>
2024-01-06 17:57:36 +0530

    updated meta tags

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -8,11 +8,25 @@ import "../styles/globals.css";
 import { ThemeProvider } from "../context/Theme";
 
 function MyApp({ Component, pageProps }) {
+  const title = "Rishabh Pathak's Portfolio";
+  const description = "A Website build to showcase my Projects and Experience";
+  const src = "https://rishabhpathak.vercel.app/preview.png";
+
   return (
     <ThemeProvider>
       <Head>
-        <title>Rishabh Pathak&apos;s Portfolio</title>
+        <title>{title}</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" />
+        <meta property="title" content={title} />
+        <meta name="description" content={description} />
+
+        <meta property="og:title" content={title} />
+        <meta property="og:description" content={description} />
+        <meta property="og:image" content={src} />
+
+        <meta property="twitter:title" content={title} />
+        <meta property="twitter:description" content={description} />
+        <meta name="twitter:image" content={src} />
       </Head>
       <Script defer src="https://code.iconify.design/1/1.0.7/iconify.min.js" />
       <Component {...pageProps} />
--- pages/_document.jsx
+++ pages/_document.jsx
@@ -1,10 +1,6 @@
 import React from "react";
 import Document, { Html, Head, Main, NextScript } from "next/document";
 
-const title = "Rishabh Pathak's Portfolio";
-const description = "A Website build to showcase my Projects and Experience";
-const src = "/preview.png";
-
 class MyDocument extends Document {
   static async getInitialProps(ctx) {
     const initialProps = await Document.getInitialProps(ctx);
@@ -14,18 +10,7 @@ class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
-        <Head>
-          <meta property="title" content={title} />
-          <meta name="description" content={description} />
-
-          <meta property="og:title" content={title} />
-          <meta property="og:description" content={description} />
-          <meta property="og:image" content={src} />
-
-          <meta property="twitter:title" content={title} />
-          <meta property="twitter:description" content={description} />
-          <meta name="twitter:image" content={src} />
-        </Head>
+        <Head />
         <body>
           <Main />
           <NextScript />
